40eda87294686ec5df2ca2309c710bdd2fcfef02
erai2 <gjjiy1169@gmail.com>
2025-09-12 21:22:47 +0900

    Delete i_web/saju_ai_web directory

--- i_web/saju_ai_web/search.js
+++
@@ -1,8 +0,0 @@
-import Fuse from './libs/fuse.min.js';
-import { getAllDocs } from './db.js';
-
-export async function searchDocs(keyword) {
-  const docs = await getAllDocs();
-  const fuse = new Fuse(docs, { keys: ["title","content"], threshold: 0.3 });
-  return keyword ? fuse.search(keyword).map(r=>r.item) : docs;
-}
